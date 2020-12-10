23fe4b3dde18793c4d43048edcc89bdd93da9732
parth patel <ppatel@qa.comgit config --global user.email ppatel@qa.com>
2020-12-10 15:23:09 +0000

    finished backend

--- ToDoProject/src/main/java/com/example/demo/persistence/domain/ToDo.java
+++ ToDoProject/src/main/java/com/example/demo/persistence/domain/ToDo.java
@@ -1,18 +1,11 @@
 package com.example.demo.persistence.domain;
 
-import java.util.List;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
-
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- ToDoProject/src/main/java/com/example/demo/persistence/repo/ToDoRepo.java
+++ ToDoProject/src/main/java/com/example/demo/persistence/repo/ToDoRepo.java
@@ -1,3 +1,4 @@
+  
 package com.example.demo.persistence.repo;
 
 import java.util.List;
